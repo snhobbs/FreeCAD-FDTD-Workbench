f77eed4bc401d4a3d4909008fe3f219b072e5512
simon <simon.hobbs@electrooptical.net>
2024-11-22 12:32:28 -0600

    adding priority setting to excitation

--- utilsOpenEMS/SettingsItem/ExcitationSettingsItem.py
+++ utilsOpenEMS/SettingsItem/ExcitationSettingsItem.py
@@ -8,8 +8,8 @@ from .SettingsItem import SettingsItem
 #
 
 class ExcitationSettingsItem(SettingsItem):
-    def __init__(self, name="", type="", sinusodial=None, gaussian=None, custom=None, dirac=None, step=None, units = "Hz"):
-        super().__init__(name=name, type=type, priority=0)
+    def __init__(self, name="", type="", sinusodial=None, gaussian=None, custom=None, dirac=None, step=None, units = "Hz", priority=0):
+        super().__init__(name=name, type=type, priority=priority)
         self.sinusodial = {'f0': 0} if sinusodial is None else sinusodial
         self.gaussian = {'f0': 0, 'fc': 0} if gaussian is None else gaussian
         self.custom = {'functionStr': '0', 'f0': 0} if custom is None else custom
@@ -20,6 +20,7 @@ class ExcitationSettingsItem(SettingsItem):
     def __str__(self):
         return f'''name={self.name}
 type={self.type}
+priority={self.priority}
 sinusodial={json.dumps(self.sinusodial)}
 gaussian={json.dumps(self.gaussian)}
 step={json.dumps(self.step)}
